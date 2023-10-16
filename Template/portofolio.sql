0fe84962da17fabd3e02eb526bd1c7018f3602f0
San <112533312+Satanizer@users.noreply.github.com>
2023-10-16 21:40:16 +0700

    Add files via upload

---
+++ Template/portofolio.sql
@@ -0,0 +1,150 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 16 Okt 2023 pada 16.36
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `portofolio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pendidikan`
+--
+
+CREATE TABLE `pendidikan` (
+  `id` int(11) NOT NULL,
+  `tahun` varchar(255) NOT NULL,
+  `gelar` varchar(255) NOT NULL,
+  `kampus` varchar(255) NOT NULL,
+  `deskripsi` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pendidikan`
+--
+
+INSERT INTO `pendidikan` (`id`, `tahun`, `gelar`, `kampus`, `deskripsi`) VALUES
+(1, '2008 - 2010', 'Master of Computer Science', 'UNIVERSITAS INDONESIA', 'Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore eu fugiat nulla pari. Excepteur sint occana inna tecat cupidatat non proident.'),
+(2, '2004 - 2008', 'Bachelor of Computer Science', 'UNIVERSITAS INDONESIA', 'Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore eu fugiat nulla pari. Excepteur sint occana inna tecat cupidatat non proident.'),
+(3, '2004 - 2008', 'Bachelor of Creative Design', 'UNIVERSITAS INDONESIA', 'Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore eu fugiat nulla pari. Excepteur sint occana inna tecat cupidatat non proident.'),
+(4, '', '', '', ''),
+(5, '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengalaman`
+--
+
+CREATE TABLE `pengalaman` (
+  `id` int(11) NOT NULL,
+  `tahun` varchar(255) NOT NULL,
+  `pekerjaan` varchar(255) NOT NULL,
+  `perusahaan` varchar(255) NOT NULL,
+  `kota` varchar(255) NOT NULL,
+  `deskripsi` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pengalaman`
+--
+
+INSERT INTO `pengalaman` (`id`, `tahun`, `pekerjaan`, `perusahaan`, `kota`, `deskripsi`) VALUES
+(1, '2018 - Present', 'CREATIVE DIRECTOR', 'SAMSUNG ELECTRONICS', 'Jakarta, Indonesia', 'Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore eu fugiat nulla pari. Excepteur sint occana inna tecat cupidatat non proident.'),
+(2, '2016 - 2018', 'ASSOCIATE DESIGN DIRECTOR', 'HOPLONY TECH LIMITED', 'Jakarta, Indonesia', 'Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore eu fugiat nulla pari. Excepteur sint occana inna tecat cupidatat non proident.'),
+(3, '2013 - 2016', 'SENIOR UI/UX DESIGNER', 'COCA-COLA COMPANY', 'Jakarta, Indonesia', 'Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore eu fugiat nulla pari. Excepteur sint occana inna tecat cupidatat non proident.'),
+(4, '2012 - 2013', 'UI/UX DESIGNER', 'GOOGLE LLC', 'Jakarta, Indonesia', 'Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore eu fugiat nulla pari. Excepteur sint occana inna tecat cupidatat non proident.'),
+(5, '2010 - 2012', 'FRONTEND DEVELOPER', 'GOOGLE LLC', 'Jakarta, Indonesia', 'Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore eu fugiat nulla pari. Excepteur sint occana inna tecat cupidatat non proident.');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `summary` varchar(255) NOT NULL,
+  `isi` text NOT NULL,
+  `hp` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `website` varchar(255) NOT NULL,
+  `gambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `nama`, `summary`, `isi`, `hp`, `email`, `website`, `gambar`) VALUES
+(1, 'Ihsan', 'I am a Professional UIUX Designer and Web developer.  Consectetur an adipisi elita, sed do eiusmod tempor incididuntut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud.', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspi unde omnis iste natus error sit voluptatem accusantium doloremque lauda ntium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam vo luptatem quia voluptas sit aspernatur aut odit aut fugit.', '08123456789', 'almond@gmail.com', 'www.almond.com', 'http://localhost/Template/assets/images/about/san.jpg'),
+(2, '', '', '', '', '', '', ''),
+(3, '', '', '', '', '', '', ''),
+(4, '', '', '', '', '', '', ''),
+(5, '', '', '', '', '', '', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `pendidikan`
+--
+ALTER TABLE `pendidikan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pengalaman`
+--
+ALTER TABLE `pengalaman`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `pendidikan`
+--
+ALTER TABLE `pendidikan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengalaman`
+--
+ALTER TABLE `pengalaman`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
